32da7794bc3f3dcbe7f2777635f95b56299343b4
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-05-14 16:25:21 +0100

    Add debug helper to main function

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"ts_inspector/analysis"
 	"ts_inspector/lsp"
+	"ts_inspector/parser"
 )
 
 func main() {
-	lsp.Start()
+	if len(os.Args) < 2 {
+		lsp.Start()
+	}
+
+	filename, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parser.InitQueries()
+	state := parser.State{}
+	state, err = parser.HandleFile(state, `file://`+filename, "typescript", 0, log.New(os.Stdout, "[ts_inspector]", log.Ldate|log.Ltime|log.Lshortfile))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	j, err := json.MarshalIndent(state, "", "  ")
+	fmt.Println(string(j))
+
+	analyses := analysis.Analyse(state[filename])
+
+	j, err = json.MarshalIndent(analyses, "", "  ")
+	fmt.Println(string(j))
 }
