71a83761e6166213b2dbcd47ae4ca264be824dc0
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-08-24 14:11:58 +0100

    Parse tsconfig baseurl too

--- ast/indexing/project.go
+++ ast/indexing/project.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"ts_inspector/utils"
 )
 
 func findProjectRoots(root string) []string {
@@ -12,11 +13,47 @@ func findProjectRoots(root string) []string {
 		log.Fatal(err)
 	}
 
-	tsConfig := readTsConfigs(root, rootFiles)
+	var tsConfig TsConfig = TsConfig{
+		CompilerOptions: tsConfigCompilerOptions{
+			BaseURL: []string{},
+		},
+		Exclude: []string{},
+		Files:   []string{},
+		Include: []string{},
+	}
+
+	readTsConfigs(root, rootFiles, &tsConfig)
+
+	if len(tsConfig.CompilerOptions.BaseURL) != 0 {
+		for _, baseUrl := range tsConfig.CompilerOptions.BaseURL {
+			basePath := filepath.Join(root, baseUrl)
+			rootFiles, err = os.ReadDir(basePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			readTsConfigs(basePath, rootFiles, &tsConfig)
+		}
+	}
 
 	for i, f := range tsConfig.Files {
 		tsConfig.Files[i] = filepath.Join(root, f)
 	}
 
-	return tsConfig.Files // TODO: handle include & exclude
+	files := []string{}
+
+	for _, baseURL := range tsConfig.CompilerOptions.BaseURL {
+		rootFiles, err := os.ReadDir(filepath.Join(root, baseURL))
+
+		filenames := []string{}
+
+		for _, rootFile := range rootFiles {
+			filenames = append(filenames, filepath.Join(root, baseURL, rootFile.Name()))
+		}
+
+		if err == nil {
+			files = append(files, utils.RemoveNonCode(filenames)...)
+		}
+	}
+
+	return append(files, tsConfig.Files...)
 }
--- ast/indexing/tsconfig.go
+++ ast/indexing/tsconfig.go
@@ -12,19 +12,36 @@ import (
 	"github.com/tailscale/hujson"
 )
 
-type TsConfig struct {
-	Exclude []string `json:"exclude"`
-	Files   []string `json:"files"`
-	Include []string `json:"include"`
+type tsConfigCompilerOptionsFile struct {
+	BaseURL string `json:"baseUrl"`
 }
 
-func parseTsConfig(filename string) TsConfig {
+type tsConfigCompilerOptions struct {
+	BaseURL []string `json:"baseUrl"`
+}
+
+// TODO: maybe there's a better way of embedding this
+type TsConfigFile struct {
+	Exclude         []string                    `json:"exclude"`
+	Files           []string                    `json:"files"`
+	Include         []string                    `json:"include"`
+	CompilerOptions tsConfigCompilerOptionsFile `json:"compilerOptions"`
+}
+
+type TsConfig struct {
+	Exclude         []string                `json:"exclude"`
+	Files           []string                `json:"files"`
+	Include         []string                `json:"include"`
+	CompilerOptions tsConfigCompilerOptions `json:"compilerOptions"`
+}
+
+func parseTsConfig(filename string) TsConfigFile {
 	content, err := utils.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var config TsConfig
+	var config TsConfigFile
 	content, err = hujson.Standardize(bytes.Trim(content, "\x00"))
 	if err != nil {
 		log.Fatal(err)
@@ -35,13 +52,7 @@ func parseTsConfig(filename string) TsConfig {
 	return config
 }
 
-func readTsConfigs(root string, rootFiles []fs.DirEntry) TsConfig {
-	var tsConfig TsConfig = TsConfig{
-		Exclude: []string{},
-		Files:   []string{},
-		Include: []string{},
-	}
-
+func readTsConfigs(root string, rootFiles []fs.DirEntry, tsConfig *TsConfig) {
 	for _, rootFile := range rootFiles {
 		rootFileName := rootFile.Name()
 		if strings.HasPrefix(rootFileName, "tsconfig.") && strings.HasSuffix(rootFileName, ".json") {
@@ -49,12 +60,14 @@ func readTsConfigs(root string, rootFiles []fs.DirEntry) TsConfig {
 			tsConfig.Exclude = append(tsConfig.Exclude, config.Exclude...)
 			tsConfig.Files = append(tsConfig.Files, config.Files...)
 			tsConfig.Include = append(tsConfig.Include, config.Include...)
+
+			if config.CompilerOptions.BaseURL != "" {
+				tsConfig.CompilerOptions.BaseURL = append(tsConfig.CompilerOptions.BaseURL, config.CompilerOptions.BaseURL)
+			}
 		}
 	}
 
 	tsConfig.Exclude = utils.RemoveNonCode(tsConfig.Exclude)
 	tsConfig.Files = utils.RemoveNonCode(tsConfig.Files)
 	tsConfig.Include = utils.RemoveNonCode(tsConfig.Include)
-
-	return tsConfig
 }
