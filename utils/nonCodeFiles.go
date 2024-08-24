525f4ac18883f48dd868749479ad5a9dfd6ea883
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-08-24 14:12:59 +0100

    Improve code file detection

--- utils/nonCodeFiles.go
+++ utils/nonCodeFiles.go
@@ -5,7 +5,7 @@ import "strings"
 func RemoveNonCode(files []string) []string {
 	var code = []string{}
 	for _, file := range files {
-		if !strings.HasSuffix(file, ".d.ts") && !strings.HasSuffix(file, ".spec.ts") {
+		if strings.HasSuffix(file, ".ts") && (!strings.HasSuffix(file, ".d.ts") && !strings.HasSuffix(file, ".spec.ts")) {
 			code = append(code, file)
 		}
 	}
