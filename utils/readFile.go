67b6c616d9619454a41d00b7f0bfdcf8bef53283
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-10-21 13:37:51 +0100

    Fix ReadFile being limited to 10kb files

--- utils/readFile.go
+++ utils/readFile.go
@@ -5,16 +5,9 @@ import (
 )
 
 func ReadFile(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		return make([]byte, 0), err
-	}
-
-	data := make([]byte, 10240)
-
-	_, err = file.Read(data)
-	if err != nil {
-		return nil, err
 	}
 
 	return data, nil
