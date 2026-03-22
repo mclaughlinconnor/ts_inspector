2e8714811c3837e53619df5aad5738d3514fe781
Connor McLaughlin <connor1211.mcl@gmail.com>
2026-03-22 17:05:48 +0000

    Add expression TCB

---
+++ parser/tcb/expression.go
@@ -0,0 +1,25 @@
+package tcb
+
+/**
+ * A `TcbOp` which renders an Angular expression (e.g. `{{foo() && bar.baz}}`).
+ *
+ * Executing this operation returns nothing.
+ */
+func handleExpression(tcb *Tcb, scope *Scope, expression *Expression, isBoundText bool) *Identifier {
+	if expression != nil {
+		expr := AstToTypescript(expression)
+
+		statement := Statement{}
+		if isBoundText {
+			statement.AddPart("\"\" + (")
+			statement.AddPart(expr)
+			statement.AddPart(");")
+		} else {
+			statement.AddPart("(")
+			statement.AddPart(expr)
+			statement.AddPart(");")
+		}
+	}
+
+	return nil
+}
