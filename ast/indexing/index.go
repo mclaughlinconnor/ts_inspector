ed5922b766a7c9eeb4b9fa310835a900a1e5f48b
Connor McLaughlin <connor1211.mcl@gmail.com>
2026-06-26 22:08:53 +0100

    Add new indexer that simply walks the root for the ts/pug files

--- ast/indexing/index.go
+++ ast/indexing/index.go
@@ -1,15 +1,43 @@
 package indexing
 
+import (
+	"io/fs"
+	"path/filepath"
+)
+
+var projectRootWalking = false
+
 func Index(rootUri string) ([]string, []string) {
 	roots, tsconfigFiles := findProjectRoots(rootUri)
 
-	imports := []string{}
-	for _, root := range roots {
-		i, e := recursivelyRetrieveImports(root, 0, 1)
-		if e == nil {
-			imports = append(imports, i...)
+	files := []string{}
+
+	if projectRootWalking == true {
+		for _, root := range roots {
+			i, e := recursivelyRetrieveImports(root, 0, 1)
+			if e == nil {
+				files = append(files, i...)
+			}
 		}
 	}
 
-	return imports, tsconfigFiles
+	filepath.WalkDir(rootUri, func(path string, d fs.DirEntry, err error) error {
+		if !d.Type().IsDir() {
+			ext := filepath.Ext(path)
+			if ext != ".ts" && ext != ".pug" {
+				return nil
+			}
+
+			files = append(files, path)
+			return nil
+		}
+
+		if d.Name() != "node_modules" {
+			return nil
+		}
+
+		return fs.SkipDir
+	})
+
+	return files, tsconfigFiles
 }
