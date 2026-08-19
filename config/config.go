ed79851bcf882af036bdc51bdb377d8b06891cf9
Connor McLaughlin <connor1211.mcl@gmail.com>
2026-08-19 16:21:45 +0100

    Add config to strip down startup

--- config/config.go
+++ config/config.go
@@ -1,13 +1,15 @@
 package config
 
+var MinimalStartup = false
+
 var Concurrency = false
 var Debug = false
-var DelayStart = false
+var DelayStart = false && !MinimalStartup
 var LSP = true
-var SemanticSearch = true
+var SemanticSearch = true && !MinimalStartup
 var SemanticSearchEnableFaiss = false
 var SemanticSearchEnableFzf = true
 var SemanticSearchEnableSqlite = true
 var SemanticSearchIncludeFileInterestingPoints = false
-var TsGo = true
+var TsGo = true && !MinimalStartup
 var TsGoExperimentalThingCaching = false
